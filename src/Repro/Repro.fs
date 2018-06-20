7d6779601e77ad70070bb100004b9a398c71ea4c
Nat Elkins <nathaniel.elkins@gmail.com>
2018-06-20 12:04:44 -0400

    Adding Newtonsoft.Json project.

--- src/Repro/Repro.fs
+++ src/Repro/Repro.fs
@@ -2,5 +2,6 @@ module Repro
 
 [<EntryPoint>]
 let main argv =
+    ReproLib.Say.hello "Nat"
     printfn "%A" argv
     0 // return an integer exit code
--- src/ReproLib/Library.fs
+++ src/ReproLib/Library.fs
@@ -1,5 +1,16 @@
 namespace ReproLib
 
 module Say =
+    open Newtonsoft.Json
+    type SayStuff = {
+        name : string
+        stuff : string        
+    }
+
     let hello name =
-        printfn "Hello %s" name
+        let sayStuff =
+            {
+                name = "Nat"
+                stuff = "whaddup"
+            }
+        printfn "Hello %s" (Newtonsoft.Json.JsonConvert.SerializeObject sayStuff)
